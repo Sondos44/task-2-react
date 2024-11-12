7deb1f27fe9de2193eb932bcf6c8dee0a9a545b0
Sondos Amr <117232826+Sondos44@users.noreply.github.com>
2024-11-12 22:39:20 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TodoList from "./Task-1&2";
+import TodoList3 from "./Task-3";
+function App() {
+  return (
+    <div className="App">
+      <h1> task 1 & 2 </h1>
+      <TodoList />
+      <h2> task 3 </h2>
+      <TodoList3 />
+    </div>
+  );
+}
+
+export default App;
